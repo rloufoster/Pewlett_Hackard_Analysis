05dba493d35984f92253c4e4e52afdd687158bf6
Robin Foster <robinlfoster@att.net>
2022-07-24 10:31:00 -0500

    updating challenge folders

--- Pewlett_Hackard_Analysis_Folder/Data_Challenge/Employee_Database_challenge.sql
+++ Pewlett_Hackard_Analysis_Folder/Data_Challenge/Employee_Database_challenge.sql
@@ -1,57 +1,47 @@
--- CHALLENGE
-
--- Number of titles retiring (with dups)
-SELECT e.emp_no,
-       e.first_name,
-       e.last_name,
-       t.title,
-       t.from_date,
-       t.to_date
+--CHALLENGE
+---- DELIVERABLE 1- Join employee and titles tables to get number of titles (with dupes)
+SELECT e.emp_no, e.first_name, e.last_name,
+	t.title, t.from_date, t.to_date
 INTO retirement_titles
 FROM employees as e
-INNER JOIN titles as t
-ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-order by e.emp_no;
+JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY e.emp_no 
 
-SELECT * FROM retirement_titles;
+SELECT * FROM retirement_titles
 
--- Use Dictinct with Orderby to remove dups
+-- Use Dictinct with Orderby to remove dupes
 SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-title
-INTO unique_titles
-FROM retirement_titles
-ORDER BY emp_no, title DESC;
+	first_name,
+	last_name,
+	title,
+	to_date
+INTO unique_titles 
+FROM retirement_titles 
+ORDER BY emp_no, to_date DESC;
 
 SELECT * FROM unique_titles;
 
--- Who is retiring by title.
-SELECT COUNT(ut.emp_no),
-ut.title
+-- How many are retiring by title?
+SELECT count(title) "count", title
 INTO retiring_titles
-FROM unique_titles as ut
-GROUP BY title 
-ORDER BY COUNT(title) DESC;
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
 
-SELECT * FROM retiring_titles;
+SELECT * FROM retiring_titles
 
--- Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program (eliminate dups)
-SELECT DISTINCT ON (e.emp_no) e.emp_no, 
-    e.first_name, 
-    e.last_name, 
-    e.birth_date,
-    de.from_date,
-    de.to_date,
-    t.title
-INTO mentorship_eligibilty
+----DELIVERABLE 2
+--What employees are eligible to participate in a mentorship program and eliminate dupes.
+SELECT DISTINCT ON (emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
 FROM employees as e
-Left outer Join dept_emp as de
+JOIN dept_emp as de
 ON (e.emp_no = de.emp_no)
-Left outer Join titles as t
+JOIN titles as t
 ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY e.emp_no;
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
 
-SELECT * FROM mentorship_eligibilty;
+SELECT * FROM mentorship_eligibility
--- README.md
+++ README.md
@@ -3,6 +3,42 @@
 
 ## Overview of the analysis:
 
+Pewlett Hackard is a large company with a high percentage of employees approaching retirement age due to the Baby Boomer generational phenomenon. Since many of these employees are in managerial and leadership roles, the impact on the company could be catastrophic. We must be prepared for the massive employee turnover by realizing it's scale in advance. This information will be crucial in creating and implimenting systems, such as the proposed Mentorship Program, to buffer the impact. The purpose of this Human Resources analysis is to determine how many employees are becoming eligible for retirement in the near future, what positions do they hold, and which employees will be eligible for the Mentorship Progarm. 
+ 
+
+### Method
+
+#### Original Data:
+
+PostgreSQL and pgADmin were used to conduct initial exploratory analases by querying the original data and creating tables.
+
+    * Departments.csv
+    * dept_emp.csv
+    * dept_manager.csv
+    * employees.csv
+    * salaries_csv
+    * titles_csv
+    
+For the purposes of this report, the following queries were performed to address our immediate questions.
+    
+    * retirement_titles.csv
+    * unique_titles.csv
+    * retiring_titles.csv
+    * mentorship eligibilty.csv
+  
+     
+## Determine how many employees are in a position to retire and their titles
+
+* Create a Retirement Titles table that holds all the titles of current employees who were born between January 1, 1952 and December 31, 1955.
+
+* Because some employees may have multiple titles in the database—for example, due to promotions— we need to create a table that contains the most recent title of each employee.
+
+* Finally, create a final table that has the number of retirement-age employees grouped by most recent job title.
+
+## Determine the employees eligible for the Mentorship Program
+
+* Create a mentorship-eligibility table that holds the current employees who were born between January 1, 1965 and December 31, 1965.
+
 ## Results:
 
 
@@ -27,3 +63,5 @@ EMBED TABLE OF TITLES!
 * Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
 
 ADD LINK HERE TO MENTORSHIP TABLE!
+
+In order for Pewlett Hackard to be best prepared, we must equip ourselves with two crucial points of information: 1.)   to create systems to maintain our competive edge
